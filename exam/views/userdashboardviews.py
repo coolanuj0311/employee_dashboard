8c674679b8fe307cd2584f0e751d3c879420a416
Anuj Pratap Singh <anujpratapsingh905@gmail.com>
2024-04-10 15:18:21 +0530

    apis merging and error handling done

--- exam/views/userdashboardviews.py
+++ exam/views/userdashboardviews.py
@@ -76,6 +76,7 @@ class CreateCourseCompletionStatusPerUserView(APIView):
         completion_status = (default=False)
         in_progress_status = (default=False)
         created_at = (auto_now_add=True)
+        agar userb or course ka set ho toh usse fir se populate na karein ek hi hona chahiye pure table me
     """
     def post(self, request):
         try:
@@ -107,6 +108,10 @@ class CreateCourseCompletionStatusPerUserView(APIView):
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 class UpdateCompleteQuizCountView(APIView):
     """
     POST request
@@ -114,6 +119,7 @@ class UpdateCompleteQuizCountView(APIView):
     while updating instance :
         completed_quiz_count = increment by 1
     """
+
     def post(self, request):
         try:
             # Extract data from request
@@ -124,20 +130,32 @@ class UpdateCompleteQuizCountView(APIView):
             if not (course_id and user_id):
                 return Response({'error': 'course_id and user_id are required'}, status=status.HTTP_400_BAD_REQUEST)
 
-            # Check if the quiz attempt history indicates completion for the user and course
-            if QuizAttemptHistory.objects.filter(course_id=course_id, enrolled_user_id=user_id, complete=True).exists():
+            # Filter quiz attempt history for completed quizzes
+            completed_quizzes = QuizAttemptHistory.objects.filter(course_id=course_id, enrolled_user_id=user_id, complete=True)
+
+            # Check if the filtered instance already exists
+            if completed_quizzes.exists() and completed_quizzes.count() == 1:
                 # Update completed_quiz_count for the corresponding record
-                quiz_score = QuizScore.objects.get(course_id=course_id, enrolled_user_id=user_id)
-                quiz_score.completed_quiz_count += 1
-                quiz_score.save()
+                quiz_score, created = QuizScore.objects.get_or_create(course_id=course_id, enrolled_user_id=user_id, defaults={'completed_quiz_count': 0})
+                if not created:  # If QuizScore instance already existed
+                    quiz_score.completed_quiz_count += 1
+                    quiz_score.save()
                 return Response({'message': 'Completed quiz count updated successfully'}, status=status.HTTP_200_OK)
             else:
-                raise QuizScore.DoesNotExist('No completed quiz found for the user and course')
-        except (QuizScore.DoesNotExist, Exception) as e:
-            if isinstance(e, QuizScore.DoesNotExist):
-                raise NotFound(detail='Quiz score record not found')
+                # No completed quizzes found or multiple completed quizzes found
+                return Response({'error': 'No completed quizzes found or multiple completed quizzes found'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
+
+
+
+
+
+from django.utils import timezone
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class CreateQuizScoreView(APIView):
     """
@@ -169,16 +187,28 @@ class CreateQuizScoreView(APIView):
             quiz_scores = []
             for course_id in course_ids:
                 # Calculate total quizzes for the course
-                total_quizzes_per_course = QuizScore.objects.filter(course_id=course_id, active=True).count()
+                total_quizzes_per_course = self.get_total_quizzes_per_course(course_id)
+
                 for user_id in user_ids:
+                    # Check if QuizScore instance already exists for this user and course
+                    existing_score = QuizScore.objects.filter(course_id=course_id, enrolled_user_id=user_id).first()
+
+                    if existing_score:
+                        continue  # Skip creation if QuizScore instance already exists
+
+                    # Create new QuizScore instance
                     quiz_score = QuizScore(
                         enrolled_user_id=user_id,
                         course_id=course_id,
                         total_quizzes_per_course=total_quizzes_per_course,
                         completed_quiz_count=0,
-                        total_score_per_course=0
+                        total_score_per_course=0,
+                        created_at=timezone.now(),
+                        updated_at=timezone.now(),
+                        active=True
                     )
                     quiz_scores.append(quiz_score)
+
             # Save quiz score records to the database
             QuizScore.objects.bulk_create(quiz_scores)
 
@@ -186,19 +216,40 @@ class CreateQuizScoreView(APIView):
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    
+
+    def get_total_quizzes_per_course(self, course_id):
+        try:
+        # Count the number of quizzes associated with the given course_id
+            total_quizzes = CourseStructure.objects.filter(course_id=course_id, content_type='quiz', active=True).count()
+            return total_quizzes
+        except Exception as e:
+        # Handle exceptions if any
+          return 0  # Return 0 in case of error
 
 
 
 
 
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UpdateTotalScorePerCourseView(APIView):
     """
     POST request
-    triggered when quiz attempt history for that course, that user have completed =true 
-    while updating instance :
+    triggered when quiz attempt history for that course, that user have completed = true 
+    while updating instance:
         total_score_per_course -> calculate for it 
+        score=current_score/question_list_order.split().count()
     """
     def post(self, request):
         try:
@@ -210,19 +261,35 @@ class UpdateTotalScorePerCourseView(APIView):
             if not (course_id and user_id):
                 return Response({'error': 'course_id and user_id are required'}, status=status.HTTP_400_BAD_REQUEST)
 
+            # Get unique instances of completed quizzes for the user and course
+            unique_quizzes = QuizAttemptHistory.objects.filter(course_id=course_id, enrolled_user_id=user_id, complete=True).order_by('quiz_id').distinct('quiz_id')
+
             # Calculate total score for the user and course
-            total_score = QuizAttemptHistory.objects.filter(course_id=course_id, enrolled_user_id=user_id, complete=True).values_list('current_score', flat=True).first()
+            total_score = 0
+            for quiz_attempt in unique_quizzes:
+                total_score += (quiz_attempt.current_score / (len(quiz_attempt.question_list_order.split(','))
+))
+
+            # Get total quizzes for the course
+            total_quizzes = QuizScore.objects.get(course_id=course_id, enrolled_user_id=user_id).total_quizzes_per_course
+           
+
+            # Calculate average score
+            if total_quizzes > 0:
+                average_score = (total_score / total_quizzes) * 100
+            else:
+                average_score = 0
 
             # Update total_score_per_course for the corresponding record
-            quiz_score = QuizScore.objects.get(course_id=course_id, enrolled_user_id=user_id)
-            quiz_score.total_score_per_course = total_score or 0  # If total_score is None, set it to 0
+            quiz_score, created = QuizScore.objects.get_or_create(course_id=course_id, enrolled_user_id=user_id, defaults={'total_score_per_course': 0})
+            quiz_score.total_score_per_course = average_score
             quiz_score.save()
 
             return Response({'message': 'Total score per course updated successfully'}, status=status.HTTP_200_OK)
-        except (QuizScore.DoesNotExist, Exception) as e:
-            if isinstance(e, QuizScore.DoesNotExist):
-                raise NotFound(detail='Quiz score record not found')
+        except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 
 
 
@@ -235,6 +302,7 @@ class UpdateCourseCompletionStatusPerUserView(APIView):
         completion_status=True and in_progress_status =False
     if total_quizzes_per_course > completed_quiz_count:
         completion_status=False and in_progress_status =True
+        
     """
     def post(self, request):
         try:
